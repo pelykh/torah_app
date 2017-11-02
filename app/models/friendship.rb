99ec1a240d53fba686319d040f6cc0f96e3328f6
Andriy Pelykh <andrew.pelykh@gmail.com>
2017-11-02 17:42:43 +0200

    Add push notifications to friendships

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,17 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: "User"
+
+  after_create :notify_friend
+
+  after_update :notify_user
+
+  private
+
+  def notify_friend
+    friend.notifications.create(
+      message: "#{user.name} want's to be your friend",
+      link: Rails.application.routes.url_helpers.user_path(user.id)
+    )
+  end
 end
