f33f681c23aa17805408a92a088e8a56f0722963
Andriy Pelykh <andrew.pelykh@gmail.com>
2017-11-13 14:54:48 +0200

    tempo

--- db/migrate/20171104160602_add_provider_to_users.rb
+++ db/migrate/20171104160602_add_provider_to_users.rb
@@ -3,7 +3,5 @@ class AddProviderToUsers < ActiveRecord::Migration[5.0]
     add_column :users, :provider, :string, default: "email", null: false
     add_column :users, :uid, :string, default: "", null: false
     add_column :users, :tokens, :string
-
-    add_index :users, :uid, unique: true
   end
 end
