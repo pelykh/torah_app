c0d3ccb3f677537896c69a0c4dd13456138861fa
Andriy Pelykh <andrew.pelykh@gmail.com>
2017-12-29 14:33:52 +0200

    Stylize phone ring div

--- app/assets/javascripts/channels/current_user.js
+++ app/assets/javascripts/channels/current_user.js
@@ -1,6 +1,4 @@
 jQuery(document).on('turbolinks:load', function() {
-  //createVideoCallDiv({user: "user", chatroom: {url: "/chatrooms/1"}});
-
   App.current_user = App.cable.subscriptions.create({
       channel: 'CurrentUserChannel'
     }, {
@@ -16,13 +14,28 @@ jQuery(document).on('turbolinks:load', function() {
 
   function createVideoCallDiv(data) {
     const div = `
+      <div class='overlay'></div>
       <div id='video-call-ring'>
-        <p>${data.user} is calling you</p>
+        <div><img src='/images/phone_ring.png' class='chatroom-avatar'></div>
+        <br>
+        <audio id="ring" src="/sounds/phone_ring.mp3" preload="auto"></audio>
+        <h4>${data.user} is calling you</h4>
         <a href='${data.chatroom.url}' class='btn btn-success'>Go to chatroom</a>
         <a id='cancel-video-call' class='btn btn-danger'>Cancel</a>
       </div>
     `
+
     $('body').append(div);
-    $('#cancel-video-call').on('click', () => $('#video-call-ring').remove());
+    document.getElementById('ring').play();
+    const closeDivTimeout = setTimeout(removeVideoCallDiv, 30000);
+    $('#cancel-video-call').on('click', () => {
+      clearTimeout(closeDivTimeout);
+      removeVideoCallDiv();
+    });
+  }
+
+  function removeVideoCallDiv() {
+    $('#video-call-ring').remove();
+    $('.overlay').remove();
   }
 });
